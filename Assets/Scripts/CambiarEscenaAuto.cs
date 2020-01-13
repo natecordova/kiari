622f77f5e295918adda6efc931ac77b44a098f80
natecordova <natecordovaortiz@hotmail.com>
2020-01-13 18:07:36 -0500

    Initial commit

---
+++ Assets/Scripts/CambiarEscenaAuto.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiarEscenaAuto : MonoBehaviour
+{
+    public float tiempo_start; //Los segundos por los quales comienza i la variable que utilizaremos para que vaya contando segundos.
+    public float tiempo_end; //Segundos que queremos que pasen para que cambie de escena
+                             // Update is called once per frame
+    string escena ="Main_Logo";
+
+
+    void Update()
+    {
+        tiempo_start += Time.deltaTime;//Función para que la variable tiempo_start vaya contando segundos.
+        if (tiempo_start >= tiempo_end) //Si pasan los segundos que hemos puesto antes...
+        {
+            transform.Translate(Vector3.forward * 9 * Time.deltaTime);
+            SceneManager.LoadScene(escena);
+        }
+    }
+}
---
+++ Assets/Scripts/MenuDesplegableController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuDesplegableController : MonoBehaviour
+{
+    public RectTransform subMenu;
+    float posFinal;
+    bool abrirMenu = true;
+    public float tiempo = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()    //En canvasMenu esta aplicado el script y en buttonMenu las tres rayas y la X están llamando a la accion
+    {
+        posFinal = Screen.width / 3; //Default 2
+        subMenu.position = new Vector3(-posFinal, subMenu.position.y, 0);
+
+    }
+
+    IEnumerator Mover(float time, Vector3 posInit, Vector3 posFin) {
+        float elapsedTime = 0;
+        while (elapsedTime < time) {
+            subMenu.position = Vector3.Lerp(posInit, posFin, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        subMenu.position = posFin;
+
+    }
+
+    void MoverMenu(float time, Vector3 posInit, Vector3 posFin){
+
+        StartCoroutine(Mover(time, posInit, posFin));
+
+    }
+
+    public void BUTTON_sub_menu (){
+        int signo = 1;
+        if (!abrirMenu)
+            signo = -1;
+        MoverMenu(tiempo, subMenu.position, new Vector3(signo * posFinal, subMenu.position.y, 0));
+        abrirMenu = !abrirMenu;
+
+
+    } 
+
+   
+}
+

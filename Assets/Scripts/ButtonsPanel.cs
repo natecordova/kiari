ddcd0a15431d93e90a61ec8a06579ff0d910dea8
jccarrer <carrera1995@hotmail.es>
2020-01-13 22:49:25 -0500

    Menu Principal
    Diseno del PopUp  en el menu principal

---
+++ Assets/Scripts/ButtonsPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsPanel : MonoBehaviour
+{
+    public GameObject Panel;
+
+    // Start is called before the first frame update
+    public void ClosePanelPopUp()
+    {
+        if(Panel != null)
+        {
+            Panel.SetActive(false);
+        }
+    }
+
+    public void ChangeScenes()
+    {
+        SceneManager.LoadScene(3);
+
+    }
+
+
+
+}

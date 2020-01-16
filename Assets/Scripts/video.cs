1647733b1ec0365afb2787e9a50a6e96768a5855
jccarrer <carrera1995@hotmail.es>
2020-01-16 17:20:23 -0500

    Histories video slides
    Srcreen Histories  doing video slides

---
+++ Assets/Scripts/video.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class video : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       // ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+    }
+
+ 
+}
